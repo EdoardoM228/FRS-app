964b542e1b5f210cbc4f525462ca1602d66175e3
sapec <sapec@sapecs-MacBook-Pro.local>
2021-07-21 14:38:03 +0300

    Validation and Dashboard

--- FRS/Pages/LoginPage.xaml.cs
+++ FRS/Pages/LoginPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿
+using System;
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
+
 namespace FRS.Pages
 {
     public partial class LoginPage : ContentPage
@@ -8,5 +11,80 @@ namespace FRS.Pages
         {
             InitializeComponent();
         }
+
+        /* public bool grayButton(Button button)
+         {
+             var email = EmailEntry.Text;
+             var password = PasswordEntry.Text;
+
+             if (email.Contains("@") & password.Length > 6)
+             {
+                 return button.IsEnabled = true;
+             }
+             else
+             {
+                 return button.IsEnabled = false;
+             }
+         }
+     }
+
+       */
+
+        public void EntryButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var email = EmailEntry.Text;
+            var password = PasswordEntry.Text;
+
+            var emailpattern = @"^\S+@\S+\.\S+$";
+
+            if (email != null)
+            {
+                if (Regex.Match(email, emailpattern).Success)
+                {
+                    LableError.Text = "Email is valid";
+                }
+                else
+                {
+                    LableError.Text = "Email is not valid";
+                }
+            }
+            else
+            {
+                LableError.Text = "You dont write email";
+            }
+
+
+            if (password != null)
+            {
+                if (password.Length > 6)
+                {
+                    LableErrorForPassword.Text = "Password is valid";
+                }
+                else
+                {
+                    LableErrorForPassword.Text = "Password is not valid";
+                }
+            }
+            else
+            {
+                LableErrorForPassword.Text = "You dont write password";
+            }
+
+            if (email != null & password != null)
+            {
+                if (Regex.Match(email, emailpattern).Success & password.Length > 6)
+                {
+                    Navigation.PushAsync(new Page2());
+                }
+            }
+        }
     }
+
+    public class Model
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
 }
